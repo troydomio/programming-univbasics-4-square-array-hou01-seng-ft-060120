b80c6a08d2b37c59e2e24926e17d7873354ca90c
Troy Domio <troyakins99@yahoo.com>
2020-05-10 23:50:27 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -5,7 +5,9 @@ def square_array(array)
   counter = 0
  
   while counter < array.length do
-    return new_array.push(array[counter]**2)
+    new_array.push(array[counter]**2)
    counter += 1
   end
+  return new_array
+  
 end
